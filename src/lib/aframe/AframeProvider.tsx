08d52c51398dae8386367f7a7e5ad566a46554a0
Jeonghun Ha <ada.lovelace@kakao.com>
2022-02-22 16:30:31 +0900

    👔 Make to disable vrModeUI

--- src/lib/aframe/AframeProvider.tsx
+++ src/lib/aframe/AframeProvider.tsx
@@ -11,7 +11,7 @@ export default function AframeProvider({ children }) {
             <ColorizeSea />
 
             {/* Scene */}
-            <Scene>{children}</Scene>
+            <Scene vrModeUI={{ enabled: false }}>{children}</Scene>
         </>
     );
 }
